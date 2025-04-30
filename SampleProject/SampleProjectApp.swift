a8d8a42af8711f35282c70ced9fc8a5076dfaf17
Sravanthi Bareddy <sravanthibareddy19@gmail.com>
2025-04-30 13:51:58 -0500

    Initial Commit

---
+++ SampleProject/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  SampleProject
+//
+//  Created by Sravanthi Busipelli on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
